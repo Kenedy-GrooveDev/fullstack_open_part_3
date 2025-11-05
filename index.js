7d09e95f4e97dcd4a37053bfed4c570f358cb061
Kenedy-GrooveDev <kenedylyimo211@gmail.com>
2025-11-06 00:42:31 +0300

    Exercise 3.4: Phonebook backend step 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -37,12 +37,16 @@ app.get("/api/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  person ? 
-    response.json(person) :
-    response.status(404).end()
-})
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  person ? response.json(person) : response.status(404).end();
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+});
 
 const PORT = 3001;
 
